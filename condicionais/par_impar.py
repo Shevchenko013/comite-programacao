9f043f90963190900111540808f3d7f2027ae8c0
Shevchenko013 <shevchenko.lacerda@gmail.com>
2025-09-03 21:49:27 -0300

    Inicio do conteudo de condicionais

---
+++ condicionais/par_impar.py
@@ -0,0 +1,7 @@
+numero = int(input("Informe um numero -> "))
+
+par = numero % 2
+if (numero % 2) == 0:
+    print("O numero é par!")
+else:
+    print("O numero é impar")    
---
+++ operadores_aritimeticos/tarefas-2.py
@@ -0,0 +1,18 @@
+#numero = int(input("Digite o numero -> "))
+#print("antecessor ->" numero = - 1, 
+# "sucessor ->" numero = sucessor = + 1)
+#print("O sucessor de", numero, "é", sucessor, O antecessor de", numero, "é", antecessor)
+
+#celcius = float(input("Digite a temperatura em celcius -> "))
+#fahrenheit = (celcius *9/5) + 35
+#print("A temperatura em fahrenheit","é", fahrenheit)
+
+#numero_1 = int(input("Digite o numero -> "))
+#numero_2 = int(input("Digite o numero 2 -> "))
+#print("O quociente inteiro -> de divisão", "é",  numero_1 // numero_2, "\n"
+#,"O resto da divisão", "é",  numero_1 % numero_2)
+
+base = float(input("Digite a base -> "))
+expoente = int(input("Digite o expoente -> "))
+resultado = base ** expoente
+print(base, "Elevado a", expoente, "é",resultado)
